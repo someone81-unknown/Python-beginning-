c36eab81874d896e2eb9ddb97f5898bcecc03b83
someone81-unknown <amuslim098a@gmail.com>
2026-08-04 22:48:40 +0500

    Implement MBBS qualification check and salary logic
    The script now checks for MBBS qualification and provides a salary output based on prior experience.

---
+++ Mbbs.py
@@ -0,0 +1,12 @@
+name=input("what is your name ")
+print("you are welcome "+name)
+qualification=input("what is your qualification? ")
+if qualification.upper()=="MBBS":
+     print("ok,let us see")
+else:
+	print("run from here")
+experience=input("do you have any prior experience (yes or no): ")
+if experience.lower()=="yes":
+    print("your salary is 150000! " +name)
+else:
+	print("run from here")
