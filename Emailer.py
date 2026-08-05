3b37cfe0b7a8c79fb7e85167eda8ad71e0c3924a
someone81-unknown <amuslim098a@gmail.com>
2026-08-05 12:15:56 +0500

    Add Emailer script to generate email addresses

---
+++ Emailer.py
@@ -0,0 +1,9 @@
+name=input("what is your name? ")
+user_name=input("username? ")
+domain="@hyprox.abc"
+year=2026
+print(f"""
+
+{"hi "}{name}{" your new email address is "} {name}{str(year)}{domain}
+
+""")
