3d38a57514f6ab20e3759c57e6cd23f2ece4d252
someone81-unknown <amuslim098a@gmail.com>
2026-08-05 14:43:12 +0500

    Add loan eligibility check in Loan2.py

---
+++ Loan2.py
@@ -0,0 +1,13 @@
+good_credit=False
+good_income=True
+if good_credit and good_income:
+	print("you're eligible for loan!")
+else:
+	print("sorry , you're not eligible for the loan!")
+print(f""" 
+{"|"*60}
+
+                 THANK FOR VISITING
+                 
+{"|"*60}                       
+""")
