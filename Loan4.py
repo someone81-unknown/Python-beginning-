e6ac11037ca42c4d39c59422a17b8ef5e7c0e013
someone81-unknown <amuslim098a@gmail.com>
2026-08-06 09:37:53 +0500

    Add eligibility check for loan approval

---
+++ Loan4.py
@@ -0,0 +1,11 @@
+good_credit=True
+no_criminal_record=True
+if good_credit and not no_criminal_record:
+	print("you are eligible!")
+print(f"""
+{"*"*60}
+
+                  THANKS FOR VISITING!
+
+{"*"*60}
+""")
