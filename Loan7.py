6331847f11e00edffdec4ca4c5ec464d2443e59e
someone81-unknown <amuslim098a@gmail.com>
2026-08-06 09:40:26 +0500

    Add temperature-based response logic

---
+++ Loan7.py
@@ -0,0 +1,8 @@
+if temperature<=50:
+	print("run from here!")
+elif 32<temperature<50:
+	print("it's a hot day")
+elif temperature>20:
+	print(("it's a cold day"))
+else:
+	print("go from this sit3!")
